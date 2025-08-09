0a258b56f04341eba9534b99fc1b7076538b0384
Doan Kieu <kieuvandoanit@gmail.com>
2025-08-09 15:00:47 +0700

    #6: Ma hoa mat khau va matching voi Bcrypt

---
+++ src/main/java/com/devteria/identity_service/dto/request/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.devteria.identity_service.dto.request;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthenticationRequest {
+    String username;
+    String password;
+}
--- src/main/java/com/devteria/identity_service/exception/ErrorCode.java
+++ src/main/java/com/devteria/identity_service/exception/ErrorCode.java
@@ -6,6 +6,7 @@ public enum ErrorCode {
     USER_EXISTS(1002, "User already exists"),
     USERNAME_INVALID(1003, "Username must be at least 3 characters"),
     PASSWORD_INVALID(1004, "Password must be at least 8 characters"),
+    USER_NOT_FOUND(1005, "User not found"),
     ;
 
     private int code;
