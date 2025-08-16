7c70f4ef5da25844a9b6daaf22f40fe3a310f4dc
Doan Kieu <kieuvandoanit@gmail.com>
2025-08-16 21:32:19 +0700

    #15: Validation nang cao, trich xuat thong tin tu annotation

--- src/main/java/com/devteria/identity_service/exception/ErrorCode.java
+++ src/main/java/com/devteria/identity_service/exception/ErrorCode.java
@@ -10,12 +10,12 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized Exception", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1001, "Invalid message key", HttpStatus.BAD_REQUEST),
     USER_EXISTS(1002, "User already exists", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND(1005, "User not found", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006, "User is not authenticated", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "You not has permission", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Invalid date of birth", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1008, "Your age must be at least {min} and max is {max}", HttpStatus.BAD_REQUEST),
     ;
 
     private final int code;
--- src/main/java/com/devteria/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devteria/identity_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.devteria.identity_service.exception;
 
 import com.devteria.identity_service.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
 
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException ex) {
@@ -39,9 +45,16 @@ public class GlobalExceptionHandler {
         String enumKey = ex.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
 
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = ex.getBindingResult()
+                    .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
         } catch (IllegalArgumentException e) {
 
         }
@@ -49,10 +62,19 @@ public class GlobalExceptionHandler {
 
         ApiResponse apiResponse = new ApiResponse();
 
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes)
+                ? mapAttribute(errorCode.getMessage(), attributes)
+                : errorCode.getMessage());
+
         apiResponse.setCode(errorCode.getCode());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 
     @ExceptionHandler(value = AccessDeniedException.class)
